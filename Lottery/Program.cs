87da1dde5564dbb75b9659895e17ffce9a7d8222
flysha.zhou <flysha.zhou@starlight-sms.com>
2019-01-04 14:43:32 +0800

    Lottery 2019 project initial commit.

---
+++ Lottery/Program.cs
@@ -0,0 +1,27 @@
+﻿using FlysEngine.Desktop;
+using FlysEngine.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (var window = new SpriteWindow())
+            {
+                RenderLoop.Run(window, () => window.Render(1, 0));
+            }
+        }
+    }
+}
